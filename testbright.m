cb20a5628d959e1c81944150192057621b489c37
Raveenthiran-Ajan <rjajanajan@gmail.com>
2026-03-13 21:22:12 +0530

    Delete testbright.m

--- testbright.m
+++
@@ -1,9 +0,0 @@
-I = imread('leena.png');
-h1 = imhist(I);
-J = brightness(I,30);
-h2 = imhist(J);
-
-subplot(2,2,1);imshow(I);hold on;title('Original Image');
-subplot(2,2,2);bar(h1);hold on;title('Original Histogram');
-subplot(2,2,3);imshow(J);hold on;title('Brightness Increased');
-subplot(2,2,4);bar(h2);title('New Histogram');
